61bfefee79b001b25fdea58c055a048347f570bb
Justas Dovydaitis <justuxlt1@gmail.com>
2020-05-25 12:31:25 +0300

    made footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import './Footer.css';
+import './Footer.scss';
 
 const Footer: React.FC = () => {
-    return <></>;
+    return (
+        <footer className="footer">
+            <p className="footer__text">
+                Copyright &copy; 2020&nbsp;
+                <Link to="https://www.linkedin.com/in/justas-dovydaitis/" className="footer__link">
+                    Justas Dovydaitis
+                </Link>
+                &nbsp;| TALECH WAREHOUSE
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
